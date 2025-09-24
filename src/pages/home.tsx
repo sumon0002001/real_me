2d2d93850936d41e1f46f2b7a0da05d400de9c25
sumon0002001 <sumon0002001@gmail.com>
2025-09-24 19:20:49 +0600

    added funcyiom to mobile

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -53,11 +53,16 @@ function Home() {
             <div className="px-4 py-2 space-y-2">
               
               <div className="pt-2 border-t border-gray-200">
-                <button className="block w-full text-left py-2 text-orange-100 hover:text-orange-600">
-                  Sign In
+                <button className="block w-full text-left py-2 text-orange-700 hover:text-orange-600">
+                   <Link to="/realme">
+             Sign in
+             </Link>
                 </button>
                 <button className="block w-full text-left py-2 bg-orange-600 text-orange-100 rounded-md mt-2 hover:bg-orange-700">
-                  Create Account
+                   <Link to="/register">
+             Create Account
+             </Link>
+                  
                 </button>
               </div>
             </div>
@@ -79,14 +84,17 @@ function Home() {
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <button className="bg-orange-600 text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-blue-700 transition-colors flex items-center justify-center"
               >
-                <a href="/realme">
-                 Get Started
-                </a>
+                
+                 <Link to="/realme">
+             Get Started
+             </Link>
                
                 <ArrowRight className="ml-2 h-5 w-5" />
               </button>
               <button className="border-2 border-orange-600 text-gray-600 px-8 py-4 rounded-lg text-lg font-semibold hover:bg-orange-50 transition-colors">
-                Learn More
+                 <Link to="/register">
+             Know More
+             </Link>
               </button>
             </div>
           </div>
